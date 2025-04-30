35955163d9f10ef7bd99c46409fd5e4796c67910
Nyx <mail@lemonandlime.work.gd>
2025-04-30 17:00:29 -0400

    delete
    Signed-off-by: Nyx <mail@lemonandlime.work.gd>

--- cogs/status.js
+++ cogs/status.js
@@ -1,19 +1,1 @@
-import os
-import discord
-
-# Read token directly from environment
-TOKEN = os.environ["TOKEN"]
-
-# Set up Discord client
-intents = discord.Intents.default()
-client = discord.Client(intents=intents)
-
-@client.event
-async def on_ready():
-    print(f"✅ Logged in as {client.user}")
-    await client.change_presence(
-        activity=discord.Activity(type=discord.ActivityType.watching, name="for your DMs")
-    )
-
-client.run(TOKEN)
 
